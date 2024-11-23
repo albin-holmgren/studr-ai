26e1f4bcfcc05637a32c997a42b395d32765c476
Albin holmgren <albin.holmgren97@gmail.com>
2024-11-23 17:03:18 +0100

    Initial commit

---
+++ remix.config.js
@@ -0,0 +1,14 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  ignoredRouteFiles: ["**/.*"],
+  serverModuleFormat: "cjs",
+  serverPlatform: "node",
+  serverMinify: false,
+  future: {
+    v2_errorBoundary: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+    v2_headers: true
+  }
+};
